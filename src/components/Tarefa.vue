f9206508891064f85b21803eec226dcb32f8a741
Giovanny Montinny de Almeida Dantas <gmontinny@gmail.com>
2022-07-19 12:33:10 -0400

    adasdasdsadsada

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,32 @@
+<template>
+  <Box>
+      <div class="columns">
+          <div class="column is-7">
+            {{ tarefa.descricao || 'Tarefa sem descrição'}}
+          </div>
+          <div class="column">
+              <Cronometro :tempo-em-segundos="tarefa.duracaoEmSegundos"/>
+          </div>
+      </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+import Cronometro from "@/components/Cronometro.vue";
+import ITarefa from "@/interfaces/ITarefa";
+import Box from "@/components/Box.vue";
+
+export default defineComponent ({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tarefa",
+  components: {Box, Cronometro},
+  props:{
+    tarefa: {
+       type: Object as PropType<ITarefa>,
+       required: true
+    }
+  }
+});
+</script>
+
---
+++ src/views/Projetos.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Projetos</h1>
+</template>
+
+<script lang="ts">
+  import {defineComponent} from "vue";
+
+  export  default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+      name: 'Projetos'
+  });
+
+</script>
+
+<style scoped>
+
+</style>
